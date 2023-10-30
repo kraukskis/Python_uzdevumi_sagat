f2e6c771bd9687e46995a0df4abfc9204ffd9123
Rolands A <97937320+kraukskis@users.noreply.github.com>
2023-10-30 10:28:44 +0200

    Update 7uzd.py

--- 7uzd.py
+++ 7uzd.py
@@ -6,3 +6,9 @@ pievienots klāt doktora nosaukums - Dr.
 
 saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
 sarakts2=[]
+
+for uzvards in saraksts1:
+  doktors="Dr. "+uzvards
+  saraksts2.append(doktors)
+
+print(saraksts2)
