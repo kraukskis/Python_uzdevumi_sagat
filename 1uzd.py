727ab49530136ea281991d38f0f1b48809d4f4fc
Rolands A <97937320+kraukskis@users.noreply.github.com>
2023-10-30 10:00:42 +0200

    Update 1uzd.py

--- 1uzd.py
+++ 1uzd.py
@@ -3,3 +3,28 @@ Uzrakstiet programmu definējot klasi,lai veselu skaitli pārveidotu par romieš
 
 """
 
+class AAA:
+  def __init__(self):
+    self.roman_skatili={
+      1:'I',
+      4:'IV',
+      5:'V',
+      9:'IX',
+      10:'X',
+      40:'XL',
+      50:'L',
+      90:'XC',
+      100:'C',
+      400:'CD',
+      500:'D',
+      900:'CM',
+      1000:'M',
+    }
+  
+  def uz_roman(self, num):
+    result="" #tuksssss
+    for value, numeral in sorted(self.roman_skaitli.items(), key=lambda x: x[0], reverse=True):
+      while num>=value:
+        result+=numeral
+        num-=value
+    return result
