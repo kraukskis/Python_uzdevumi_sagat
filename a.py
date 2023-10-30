263be7b9a34bee26147e8b0be8bc3421182f70bf
Rolands A <97937320+kraukskis@users.noreply.github.com>
2023-10-30 09:10:37 +0200

    Create a.py

---
+++ a.py
@@ -0,0 +1,14 @@
+#pritons jeee
+class PAKARIES:
+  def __init__(self,vards, vecums):
+    self.vards=vards
+    self.vecums=vecums
+
+#pritons uijjjj
+  def uijj(self):
+    print(self.vards, self.vecums)
+
+#obejkts or sum
+k=PAKARIES()
+k.set("Andrejs", 24)
+k.uijj()
